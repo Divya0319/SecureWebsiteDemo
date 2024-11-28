fd3ace9d326d13f804653314786b496e6837675f
divya0319 <divygupta0319@gmail.com>
2024-11-28 11:37:24 +0530

    Initial commit with index page added, and beanstalk config file added

---
+++ src/main/java/com/fastturtle/securewebsitedemo/SecureWebsiteDemoApplication.java
@@ -0,0 +1,13 @@
+package com.fastturtle.securewebsitedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureWebsiteDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureWebsiteDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fastturtle/securewebsitedemo/SecureWebsiteDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastturtle.securewebsitedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureWebsiteDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
