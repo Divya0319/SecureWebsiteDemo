131e3298e8e59246561d8bb13f8543f3d68918a0
Divya Gupta <divygupta0319@gmail.com>
2024-12-15 17:44:14 +0530

    Changed encoding to utf-8 now, for properties file, to support emojis and hindi text

--- src/main/java/com/fastturtle/securewebsitedemo/controllers/LanguageController.java
+++ src/main/java/com/fastturtle/securewebsitedemo/controllers/LanguageController.java
@@ -1,8 +1,6 @@
 package com.fastturtle.securewebsitedemo.controllers;
 
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,9 +10,6 @@ import java.util.Locale;
 
 @Controller
 public class LanguageController {
-
-    @Autowired
-    private MessageSource messageSource;
 
     @RequestMapping("/setLanguage")
     public String setLanguage(@RequestParam("lang") String lang, HttpServletRequest request) {
