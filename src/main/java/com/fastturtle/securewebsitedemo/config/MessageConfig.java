72f693681750bc349dfdede157b68858f938262b
divya0319 <divygupta0319@gmail.com>
2024-12-17 20:39:24 +0530

    Working on Youtube video content showing hindi and english depending on chosen language, also working on storing preferred language in cookie and loading it next time

--- src/main/java/com/fastturtle/securewebsitedemo/config/MessageConfig.java
+++ src/main/java/com/fastturtle/securewebsitedemo/config/MessageConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.support.ReloadableResourceBundleMessageSource
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
@@ -25,11 +26,15 @@ public class MessageConfig implements WebMvcConfigurer {
 
     @Bean
     public LocaleResolver localeResolver() {
-        SessionLocaleResolver slr = new SessionLocaleResolver();
-        slr.setDefaultLocale(Locale.ENGLISH);
-        slr.setLocaleAttributeName("current.locale");
-        slr.setTimeZoneAttributeName("current.timezone");
-        return slr;
+//        SessionLocaleResolver slr = new SessionLocaleResolver();
+//        slr.setDefaultLocale(Locale.ENGLISH);
+//        slr.setLocaleAttributeName("current.locale");
+//        slr.setTimeZoneAttributeName("current.timezone");
+//        return slr;
+        CookieLocaleResolver cookieLocaleResolver = new CookieLocaleResolver("preferredLanguage");
+        cookieLocaleResolver.setDefaultLocale(Locale.ENGLISH); // Default language
+        cookieLocaleResolver.setCookieMaxAge(3600); // Expiry time in seconds (1 hour here)
+        return cookieLocaleResolver;
     }
 
     @Bean
--- src/main/java/com/fastturtle/securewebsitedemo/controllers/LanguageController.java
+++ src/main/java/com/fastturtle/securewebsitedemo/controllers/LanguageController.java
@@ -2,15 +2,21 @@ package com.fastturtle.securewebsitedemo.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class LanguageController {
 
     @GetMapping("/language")
-    public ModelAndView language() {
+    public ModelAndView language(@RequestParam(value = "lang", defaultValue = "en") String lang) {
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("index");
+        if(lang.equals("hi") || lang.equals("hne"))
+            modelAndView.addObject("chosenLanguage", "hi");
+        else {
+            modelAndView.addObject("chosenLanguage", "en");
+        }
 
         return modelAndView;
     }
